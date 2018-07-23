6be389e1f900fb786d7703df8e362cdd7ab916d3
Rahul Gurung <gurungrahul2@gmail.com>
2018-07-23 19:37:36 +0530

    added templates

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, session, render_template, url_for, request, redirect, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,4 +10,10 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-return "Project 2: TODO"
+    return render_template("index.html")
+
+@socketio.on("submit vote")
+def vote(data):
+    selection = data["selection"]
+    votes[selection] += 1
+    emit("vote totals", votes, broadcast=True)
